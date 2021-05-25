d05b3885f1eada4387a0d37bb748dd464e81cb5e
Sanket Sinha <sinhasanket3@gmail.com>
2021-05-25 10:52:41 +0545

    Authentication Service

--- technical-service/src/main/java/com/upgrad/technical/service/business/AuthenticationService.java
+++ technical-service/src/main/java/com/upgrad/technical/service/business/AuthenticationService.java
@@ -20,14 +20,36 @@ public class AuthenticationService {
 
     @Autowired
     private PasswordCryptographyProvider CryptographyProvider;
-
+    //implementing business logic for authentication
     @Transactional(propagation = Propagation.REQUIRED)
     public UserAuthTokenEntity authenticate(final String username, final String password) throws AuthenticationFailedException {
+        //fetching user with given uuid
         UserEntity userEntity = userDao.getUserByEmail(username);
-
+        //throwing user not found error message
+        if(userEntity==null)
+            throw new AuthenticationFailedException("ATH-001","User with email not found");
+        //encrypting password , which was got from authorization header
         final String encryptedPassword = CryptographyProvider.encrypt(password, userEntity.getSalt());
-
+        if(encryptedPassword.equals(userEntity.getPassword())){
+            JwtTokenProvider jwtTokenProvider=new JwtTokenProvider(encryptedPassword);
+            UserAuthTokenEntity userAuthToken=new UserAuthTokenEntity();
+            userAuthToken.setUser(userEntity);
+            final ZonedDateTime now=ZonedDateTime.now();
+            final ZonedDateTime expiresAt=now.plusHours(8);
+            //generating access token
+            userAuthToken.setAccessToken(jwtTokenProvider.generateToken(userEntity.getUuid(), now, expiresAt));
+            userAuthToken.setLoginAt(now);
+            userAuthToken.setExpiresAt(expiresAt);
+            userDao.createAuthToken(userAuthToken); //updating user auth tokens table
+            userEntity.setLastLoginAt(now);
+            userDao.updateUser(userEntity); //updating user's last login in user table
+            return userAuthToken; //returning authenticated user auth token
+        }else {
+            //throwing password failed error message
+            throw new AuthenticationFailedException("ATH-002","Password failed");
+        }
     }
 }
+
 
 
